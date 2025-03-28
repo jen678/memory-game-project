c65579637e2a88a42117cc8efda14860da109ac6
jen <jenni_789@hotmail.co.uk>
2025-03-28 16:15:32 +0000

    mmemory game project

---
+++ memory/app.js
@@ -0,0 +1,63 @@
+// Seudo code for MEMORY GAME
+
+//  -------VARIABLES------
+//  - 
+// Provide HTML link to access game page
+// clearly display the name of the game 'Memory'
+// Add a brief but clear descriptio of 'How to play'
+// Add a 'start playing' button at the bottom of the grid 
+
+// SHOW GRID LAYOUT 
+// (5x4) grid, centered
+// grid will appear once the 'start playing' button has been clicked.
+// There is a total of 20 squares
+// ALL square will have an img randomly allocated to it 
+// 10 img will be prvided (2 of each will be added to make it a total of 20 beig displayed on the grid behind cover)
+// ALL the squares will have an open and close function 
+
+// GAME CONCEPT 
+// This is a game between two players (the user & the clock)
+// player 1 will Select 'two' random squares to check if they both contain the same image inside
+// IF the two squares they click contain the same image, they will remain there
+// IF the player has not selected the ccorecct two images, the squares will close up, and it will allow the user to click on another image theey can match instead
+// The timer will track how long it is taking them to complete mattching the images/squares
+// There is s speccific time OF 60 SECONDS that will be displayed for the user to complete matching the images
+
+// YOU DID IT! / END OF GAME message
+// message will only appear once all images are paired correctly
+// The timer will show the user how long it took them
+
+
+// --------CACHED ELEMENT REFERENCES----------
+
+
+
+
+
+
+
+
+
+
+// ----------FUNCTIONS----------
+
+
+
+
+
+
+
+
+
+
+
+// -----------EVENT LISTENNERS-----------
+
+
+
+
+
+
+
+
+// 
